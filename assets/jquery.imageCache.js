9de7740696436e8b3b1e1efddb99697f40a5efc0
Glavan Dumitru <dumitru@fetelespun.ro>
2011-04-26 20:26:22 +0200

    Canvas encoder experimental

--- assets/jquery.imageCache.js
+++ assets/jquery.imageCache.js
@@ -16,16 +16,43 @@
 *   http://www.opensource.org/licenses/mit-license.php
 *   http://www.gnu.org/licenses/gpl.html
 *
+* for (v in localStorage) {delete localStorage[v]} - quick clear localStorage when developing
 */
 ;(function ($) {
 	$.fn.imageCache = function (options) {
 		this.config = {
-			base64ImageEncoderPath: 'get_image.php?id='
+			base64ImageEncoderPath: 'get_image.php?id=',
+			canvasEncoder: true // Experimental
 		};
 		$.extend(this.config, options);
+		
+		// Check for canvas support
+		this.config.canvasEncoder = typeof HTMLCanvasElement != undefined ? this.config.canvasEncoder : false;
 
 		var self = this;
-		var $self = $(this);
+		
+		var getBase64Image = function (img) {
+			try {
+				var canvas = document.createElement('canvas');
+				canvas.width = img.width;
+				canvas.height = img.height;
+
+				var ctx = canvas.getContext('2d');
+				ctx.drawImage(img, 0, 0);
+				
+				var imgType = img.src.match(/(jpg|jpeg|png)/);
+				if (imgType.length) {
+					imgType = imgType[0] == 'jpg' ? 'jpeg' : imgType[0];
+				} else {
+					throw 'Invalid image type for canvas encoder.';
+				}console.log(imgType,canvas.toDataURL('image/' + imgType));
+
+				return canvas.toDataURL('image/' + imgType);
+			} catch (e) {
+				console && console.log(e);
+				return 'error';
+			}
+		}
 
 		$(document).ready(function () {
 			$(self).each(function (i, img) {
@@ -39,15 +66,19 @@
 						$img.attr('src', src);
 						if (localStorage[src] !== 'pending') {
 							localStorage[src] = 'pending';
-							$.ajax({
-								url: self.config.base64ImageEncoderPath + src,
-								success: function (data) {
-									localStorage[src] = data;
-								},
-								error: function () {
-									localStorage[src] = 'error';
-								}
-							});
+							if (self.config.canvasEncoder) {
+								localStorage[src] = getBase64Image(img);
+							} else {
+								$.ajax({
+									url: self.config.base64ImageEncoderPath + src,
+									success: function (data) {
+										localStorage[src] = data;
+									},
+									error: function () {
+										localStorage[src] = 'error';
+									}
+								});
+							}							
 						}
 					}
 				}
